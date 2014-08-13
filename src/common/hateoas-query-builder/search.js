80a49180a7140c0af196a3a9ff19034507499ce8
Nelson Lee <nlee@uhnresearch.ca>
2014-08-13 17:28:09 -0400

    Initial commit for hateoas-query-builder

---
+++ src/common/hateoas-query-builder/search.js
@@ -0,0 +1,37 @@
+angular.module('hateoas.queryBuilder',[])
+  .directive('queryBuilder', ['$http',
+  
+  function queryBuilder() {
+    //@link https://github.com/balderdashy/waterline-criteria/blob/master/lib/filters/where.js
+    function searchController ($scope, $http) {
+      $scope.groupOperators = [ 'and', 'or' ];
+      $scope.criteria = [
+        'not',
+        'equals',
+        'greaterThan',
+        'greaterThanOrEqual',
+        'lessThan',
+        'lessThanOrEqual',
+        'startsWith',
+        'endsWith',
+        'contains',
+        'like'];
+    }
+
+    function postLink (scope, element, attribute, controller) {
+      var response = scope.response();
+      var api = response.href;
+      scope.fields = response.template.data;
+    }
+
+    return {
+      restrict: 'E',
+      scope: {
+        fields: '&',
+        callback: '&'
+      },
+      link: postLink
+    }
+  }
+
+  ]);
