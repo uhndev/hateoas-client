125b1713fbc5f1805fb506615f46f613601bd782
Kevin Chan <kvii.chan@gmail.com>
2015-11-24 16:53:32 -0500

    chore(subject_portal): minor refactor of init code

--- src/app/subjectportal/subjectportal.controller.js
+++ src/app/subjectportal/subjectportal.controller.js
@@ -10,45 +10,41 @@
   function SubjectPortalController(TableParams, SailsNgTable, StudySubjects, ScheduleSubjects) {
     var vm = this;
 
+    // bindable variables
     vm.scheduleQuery = { 'where' : {} };
     vm.studyQuery = { 'where': {} };
     vm.scheduleSubjects = [];
     vm.studySubjects = [];
+
+    // bindable methods
 
     init();
 
     ///////////////////////////////////////////////////////////////////////////
 
     function init() {
+      vm.studyTableParams = getPortalTable('study');
+      vm.scheduleTableParams = getPortalTable('schedule');
+    }
 
-      vm.studyTableParams = new TableParams({
+    function getPortalTable(type) {
+      var settings = {};
+      settings.query = (type == 'study') ? vm.studyQuery : vm.scheduleQuery;
+      settings.resource = (type == 'study') ? StudySubjects : ScheduleSubjects;
+      settings.value = (type == 'study') ? vm.studySubjects : vm.scheduleSubjects;
+
+      return new TableParams({
         page: 1,
         count: 10,
         sorting: { 'studyName': 'ASC' }
       }, {
-        groupBy: "studyName",
+        groupBy: 'studyName',
         getData: function($defer, params) {
-          var api = SailsNgTable.parse(params, vm.studyQuery);
-          StudySubjects.query(api,function (studySubjects) {
-            vm.studySubjects = angular.copy(studySubjects);
-            params.total(studySubjects.total);
-            $defer.resolve(studySubjects.items);
-          });
-        }
-      });
-
-      vm.scheduleTableParams = new TableParams({
-        page: 1,
-        count: 10,
-        sorting: { 'studyName': 'ASC' }
-      }, {
-        groupBy: "studyName",
-        getData: function($defer, params) {
-          var api = SailsNgTable.parse(params, vm.scheduleQuery);
-          ScheduleSubjects.query(api, function (scheduleSubjects) {
-            vm.scheduleSubjects = angular.copy(scheduleSubjects);
-            params.total(scheduleSubjects.total);
-            $defer.resolve(scheduleSubjects.items);
+          var api = SailsNgTable.parse(params, (type == 'study') ? vm.studyQuery : vm.scheduleQuery);
+          settings.resource.query(api, function (resource) {
+            settings.value = angular.copy(resource);
+            params.total(resource.total);
+            $defer.resolve(resource.items);
           });
         }
       });
