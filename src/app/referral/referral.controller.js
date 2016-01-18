235d5ec6476685616be06344c41bfe793ee49f7b
Kevin Chan <kvii.chan@gmail.com>
2016-01-18 12:13:27 -0500

    Added referralContact field to simple-table on referral overview page

--- src/app/referral/referral.controller.js
+++ src/app/referral/referral.controller.js
@@ -32,25 +32,27 @@
         vm.referral = angular.copy(data.items);
 
         vm.selectedProgram = data.items.program;
+        vm.selectedSite = data.items.site;
         vm.selectedPhysician = data.items.physician;
 
         var clientData = _.pick(vm.referral.clientcontact, 'MRN', 'displayName', 'dateOfBirth');
-        var referralData = _.pick(vm.referral, 'program', 'site', 'physician', 'referralDate', 'accidentDate', 'sentDate', 'receiveDate', 'dischargeDate');
+        var referralData = _.pick(vm.referral, 'program', 'site', 'physician', 'referralContact', 'referralDate', 'accidentDate', 'sentDate', 'receiveDate', 'dischargeDate');
 
         // referral info panel
         vm.referralInfo = {
           rows: {
-            'MRN': { title: 'COMMON.MODELS.CLIENT.MRN', type: 'text' },
-            'displayName': { title: 'COMMON.MODELS.PERSON.NAME', type: 'text' },
-            'dateOfBirth': { title: 'COMMON.MODELS.PERSON.DATE_OF_BIRTH', type: 'date' },
-            'program': { title: 'Program', type: 'program' },
-            'site': { title: 'Site', type: 'site' },
-            'physician': { title: 'Physician', type: 'physician' },
-            'referralDate': { title: 'COMMON.MODELS.REFERRAL.REFERRAL_DATE', type: 'date' },
-            'accidentDate': { title: 'COMMON.MODELS.REFERRAL.ACCIDENT_DATE', type: 'date' },
-            'sentDate': { title: 'COMMON.MODELS.REFERRAL.SENT_DATE', type: 'date' },
-            'receiveDate': { title: 'COMMON.MODELS.REFERRAL.RECEIVE_DATE', type: 'date' },
-            'dischargeDate': { title: 'COMMON.MODELS.REFERRAL.DISCHARGE_DATE', type: 'date' }
+            'MRN': {title: 'COMMON.MODELS.CLIENT.MRN', type: 'text'},
+            'displayName': {title: 'COMMON.MODELS.PERSON.NAME', type: 'text'},
+            'dateOfBirth': {title: 'COMMON.MODELS.PERSON.DATE_OF_BIRTH', type: 'date'},
+            'program': {title: 'Program', type: 'program'},
+            'site': {title: 'Site', type: 'site'},
+            'physician': {title: 'Physician', type: 'physician'},
+            'referralContact': {title: 'Referral Contact', type: 'employee'},
+            'referralDate': {title: 'COMMON.MODELS.REFERRAL.REFERRAL_DATE', type: 'date'},
+            'accidentDate': {title: 'COMMON.MODELS.REFERRAL.ACCIDENT_DATE', type: 'date'},
+            'sentDate': {title: 'COMMON.MODELS.REFERRAL.SENT_DATE', type: 'date'},
+            'receiveDate': {title: 'COMMON.MODELS.REFERRAL.RECEIVE_DATE', type: 'date'},
+            'dischargeDate': {title: 'COMMON.MODELS.REFERRAL.DISCHARGE_DATE', type: 'date'}
           },
           tableData: _.objToPair(_.merge(clientData, referralData))
         };
@@ -60,7 +62,7 @@
       });
     }
 
-	  /**
+    /**
      * updateReferral
      * @description Saves program and physician selections for a selected referral
      */
@@ -72,8 +74,8 @@
           physician: vm.selectedPhysician
         });
         referral
-          .$update({ id: vm.referral.id })
-          .then(function() {
+          .$update({id: vm.referral.id})
+          .then(function () {
             toastr.success('Updated referral for client: ' + vm.referral.clientcontact.displayName, 'Triage');
             init();
           });
