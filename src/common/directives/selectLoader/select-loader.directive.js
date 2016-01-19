051a9ca4163391ad6380791382461745cfcc695b
Kevin Chan <kvii.chan@gmail.com>
2016-01-19 10:16:40 -0500

    initial commit, require ngmodel in directive and check for ng-change on parent's isolate scope

--- src/common/directives/selectLoader/select-loader.directive.js
+++ src/common/directives/selectLoader/select-loader.directive.js
@@ -1,29 +1,39 @@
-(function() {
-	'use strict';
+(function () {
+  'use strict';
 
-	angular
-		.module('dados.common.directives.selectLoader', [
-			'dados.common.directives.selectLoader.controller'
-		])
-		.directive('selectLoader', selectLoader);
+  angular
+    .module('dados.common.directives.selectLoader', [
+      'dados.common.directives.selectLoader.controller'
+    ])
+    .directive('selectLoader', selectLoader);
 
-	selectLoader.$inject = [];
+  selectLoader.$inject = ['$timeout'];
 
-	function selectLoader() {
-		return {
-			restrict: 'E',
-			scope: {
-				url: '@',
+  function selectLoader($timeout) {
+    return {
+      restrict: 'E',
+      require: 'ngModel',
+      scope: {
+        url: '@',
         query: '=',
-				isAtomic: '=',
-				isDisabled: '=',
-				values: '=',
+        isAtomic: '=',
+        isDisabled: '=',
+        values: '=ngModel',
         labels: '@'
-			},
-			templateUrl: 'directives/selectLoader/select-loader.tpl.html',
-			controller: 'SelectController',
-			controllerAs: 'select',
-			bindToController: true
-		};
-	}
+      },
+      templateUrl: 'directives/selectLoader/select-loader.tpl.html',
+      controller: 'SelectController',
+      controllerAs: 'select',
+      bindToController: true,
+      link: function(scope, element, attributes, ngModel) {
+        // from model -> view
+        ngModel.$render = function() {
+          scope.values = ngModel.$viewValue;
+          if (attributes.ngChange) {
+            scope.$parent.$eval(attributes.ngChange);
+          }
+        };
+      }
+    };
+  }
 })();
