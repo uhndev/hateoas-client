02c9059136f66e5156656b3a32b6c870f3e6f8ef
Kevin Chan <kvii.chan@gmail.com>
2016-02-03 13:27:01 -0500

    added fixes for select-loader, disabled cache, added infinite scroll capability

--- src/common/directives/selectLoader/select-loader.service.js
+++ src/common/directives/selectLoader/select-loader.service.js
@@ -1,66 +1,53 @@
-(function() {
+(function () {
   'use strict';
 
   angular
-  .module('dados.common.directives.selectLoader.service', [])
-  .service('SelectService', SelectService);
+    .module('dados.common.directives.selectLoader.service', ['sails.io'])
+    .service('SelectService', SelectService);
 
-  SelectService.$inject = ['$http', '$q', '$location'];
+  SelectService.$inject = ['$sailsSocket'];
 
   /**
-     * SelectService
-     * @description Service for optimizing $http calls when populating resource based dropdowns.  If multiple /user
-     *              dropdowns exist on a page, there is no sense in fetching the same data several times, so if the
-     *              $location is the same, fetch data from cache.
-     * @param $http
-     * @param $q
-     * @param $location
-     * @constructor
-  	 */
-  function SelectService($http, $q, $location) {
-    var cache = {};
-    var currentLocation = null;
+   * SelectService
+   * @description Service for optimizing $http calls when populating resource based dropdowns.  If multiple /user
+   *              dropdowns exist on a page, there is no sense in fetching the same data several times, so if the
+   *              $location is the same, fetch data from cache.
+   * @param $http
+   * @constructor
+   */
+  function SelectService($sailsSocket) {
+    this.loadSelect = function (url, baseQuery, baseOverride, query) {
+      var httpConfig = {
+        url: url,
+        method: 'GET'
+      };
 
-    this.loadSelect = function(url, baseQuery, query) {
-      var deferred;
-      if (currentLocation !== $location.path()) {
-        cache = {};
-      }
-
-      if (!cache[url] || (cache[url] && currentLocation !== $location.path())) {
-        var httpConfig = {
-          url: url,
-          method: 'GET'
+      if (query) { // if query given use where contains clause on search string
+        httpConfig.params = {
+          where: {
+            displayName: {'contains': query}
+          }
         };
-
-        if (query) { // if query given use where contains clause on search string
-          httpConfig.params = {
-            where: {
-              displayName: {'contains': query}
-            }
-          };
-          if (baseQuery) { // if given baseQuery and some additional search string
-            _.merge(httpConfig.params.where, baseQuery);
-          }
-        } else {
-          if (baseQuery) { // otherwise if no query given and baseQuery given
-            httpConfig.params = {
-              where: baseQuery
-            };
-          }
+        if (baseQuery) { // if given baseQuery and some additional search string
+          _.merge(httpConfig.params.where, baseQuery);
         }
-
-        cache[url] = $http(httpConfig).then(function(response) {
-          return response.data.items;
-        });
-        currentLocation = $location.path();
-        return cache[url];
       } else {
-        deferred = $q.defer();
-        deferred.resolve(cache[url]);
-        currentLocation = $location.path();
-        return deferred.promise;
+        if (baseQuery) { // otherwise if no query given and baseQuery given
+          httpConfig.params = {
+            where: baseQuery
+          };
+        }
       }
+
+      // extend config with any baseOverrides
+      _.merge(httpConfig.params, baseOverride);
+
+      return $sailsSocket.get(httpConfig.url, {params: httpConfig.params}).then(function (response) {
+        return {
+          total: response.data.total,
+          items: response.data.items
+        };
+      });
     };
   }
 })();
