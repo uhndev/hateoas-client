18f988d2db4d97c32af08ff1ebcb204c685fb22a
Calvin Su <calvin.su@uhn.ca>
2016-02-29 11:16:21 -0500

    for update the client

--- src/common/directives/notesEditor/altum-note.directive.js
+++ src/common/directives/notesEditor/altum-note.directive.js
@@ -5,7 +5,7 @@
   'use strict';
 
   angular
-      .module('altum.note', ['altum.note.controller', 'ngCkeditor'])
+      .module('altum.note', ['altum.note.controller'])
       .directive('altumNote', altumNote);
 
   altumNote.$inject = [];
