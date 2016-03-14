598ccd969060098c836046d9363ff6421bf45553
Calvin Su <calvin.su@uhn.ca>
2016-03-14 15:20:20 -0400

    debug client register new function

--- src/app/clientregister/client.register.controller.js
+++ src/app/clientregister/client.register.controller.js
@@ -2,90 +2,88 @@
  * Created by calvinsu on 2016-01-22.
  */
 (function () {
-    'use strict';
+  'use strict';
 
-    angular
-        .module('altum.client.register.controller', [])
-        .controller('ClientRegisterController', ClientRegisterController);
+  angular
+      .module('altum.client.register.controller', [])
+      .controller('ClientRegisterController', ClientRegisterController);
 
-    ClientRegisterController.$inject = ['resolvedClient', 'AltumAPIService', '$location', '$state', 'toastr'];
+  ClientRegisterController.$inject = ['resolvedClient', 'AltumAPIService', '$location', '$state', 'toastr'];
 
-    /* @ngInject */
-    function ClientRegisterController(resolvedClient, AltumAPIService, $location, $state, toastr) {
-        var vm = this;
+  /* @ngInject */
+  function ClientRegisterController(resolvedClient, AltumAPIService, $location, $state, toastr) {
+    var vm = this;
 
-        vm.client = resolvedClient;
+    vm.client = resolvedClient;
 
-        vm.cancelClientAdd = cancelClientAdd;
-        vm.addEmployee = addEmployee;
-        vm.save = save;
-        vm.clientAdd = clientAdd;
+    vm.cancelClientAdd = cancelClientAdd;
+    vm.addEmployee = addEmployee;
+    vm.save = save;
+    vm.clientAdd = clientAdd;
 
-        init();
+    init();
 
-        ////////////////
+    ////////////////
 
-        function init() {
-            if (vm.client.id) {
-                AltumAPIService.Person.get({
-                    id: vm.client.personid,
-                    populate: ['address', 'familyDoctor', 'employments']
-                }, function (data) {
-                    vm.client.person = data;
-                    if (data.address.city){
-                    vm.client.person.address.city = AltumAPIService.City.get({id: data.address.city});}
-                    vm.client.person.employments = AltumAPIService.Employee.query({
-                        person: vm.client.personid,
-                        populate: 'company'
-                    });
-                    if (data.familyDoctor.person){
-                    vm.client.person.familyDoctor.person = AltumAPIService.Person.get({id: data.familyDoctor.person});}
-                    if(data.primaryEmergencyContact){
-                    vm.client.person.primaryEmergencyContact = AltumAPIService.EmergencyContact.get({id: data.primaryEmergencyContact});}
-                   // vm.client.person.emergencyContacts = AltumAPIService.EmergencyContact.query({person: vm.client.personid});
+    function init() {
+      if (vm.client.id) {
+        AltumAPIService.Person.get({
+          id: vm.client.personid,
+          populate: ['address', 'familyDoctor', 'employments']
+        }, function (data) {
+          vm.client.person = data;
+          if (data.address.city) {
+          vm.client.person.address.city = AltumAPIService.City.get({id: data.address.city});}
+          vm.client.person.employments = AltumAPIService.Employee.query({
+            person: vm.client.personid,
+            populate: 'company'
+          });
+          if (data.familyDoctor.person) {
+          vm.client.person.familyDoctor.person = AltumAPIService.Person.get({id: data.familyDoctor.person});}
+          if (data.primaryEmergencyContact) {
+          vm.client.person.primaryEmergencyContact = AltumAPIService.EmergencyContact.get({id: data.primaryEmergencyContact});}
+          // vm.client.person.emergencyContacts = AltumAPIService.EmergencyContact.query({person: vm.client.personid});
 
-                });
-            }
-        }
-
-
-        function addEmployee() {
-            alert('employee function called');
-            var employee = {};
-            vm.client.person.employments.push(employee);
-        }
-
-        function cancelClientAdd() {
-            vm.client = null;
-            $location.path('/client');
-            $state.go('hateoas');
-        }
-
-        function save() {
-            //update client
-            vm.client.$update()
-                .then(function (resp) {
-                        toastr.success('Updated client ' + resp.id + '!');
-                    },
-                    function (err) {
-                        toastr.error('Updating client ' + 'failed. ' + err);
-                    });
-        }
-
-        function clientAdd() {
-            var newClient = new AltumAPIService.Client();
-
-            newClient.$save()
-                .then(function (resap) {
-                        toastr.success('New client created');
-                    },
-                    function (err) {
-                        toastr.error("failed to created the client");
-                    });
-        }
-
-
+        });
+      }
     }
+
+    function addEmployee() {
+      alert('employee function called');
+      var employee = {};
+      vm.client.person.employments.push(employee);
+    }
+
+    function cancelClientAdd() {
+      vm.client = null;
+      $location.path('/client');
+      $state.go('hateoas');
+    }
+
+    function save() {
+      //update client
+      vm.client.$update()
+                .then(function (resp) {
+                  toastr.success('Updated client ' + resp.id + '!');
+                },
+                    function (err) {
+                      toastr.error('Updating client ' + 'failed. ' + err);
+                    });
+    }
+
+    function clientAdd() {
+      var newClient = new AltumAPIService.Client();
+
+      newClient.$save()
+                .then(function (resap) {
+                  toastr.success('New client created');
+                },
+                    function (err) {
+                      toastr.error('failed to created the client');
+                    });
+    }
+
+  }
 
 })();
 
--- src/app/clientregister/client.register.module.js
+++ src/app/clientregister/client.register.module.js
@@ -1,51 +1,51 @@
 ﻿(function () {
-    'use strict';
-    angular.module('altum.client.register', [
-            'dados.common.services.altum',
-            'altum.client.register.controller'
-            // 'altum.client.register.factory'
-        ])
+  'use strict';
+  angular.module('altum.client.register', [
+          'dados.common.services.altum',
+          'altum.client.register.controller'
+          // 'altum.client.register.factory'
+      ])
         .config(['$stateProvider', function ($stateProvider) {
 
-            $stateProvider
+          $stateProvider
                 .state('newClient', {
-                    //  authenticate: true,
-                    url: '/newClient',
-                    data: {pageTitle: 'Add Client'},
-                    resolve: {
-                        resolvedClient: function () {
-                            return {
-                                person: {
-                                    address: {
-                                        city: {}
-                                    },
-                                    familyDoctor: {
-                                        person: {}
-                                    },
-                                    employments: [],
-                                    primaryEmergencyContact: {},
-                                    emergencyContacts: []
-                                }
-                            };
+                  //  authenticate: true,
+                  url: '/newClient',
+                  data: {pageTitle: 'Add Client'},
+                  resolve: {
+                    resolvedClient: function () {
+                      return {
+                        person: {
+                          address: {
+                            city: {}
+                          },
+                          familyDoctor: {
+                            person: {}
+                          },
+                          employments: [],
+                          primaryEmergencyContact: {},
+                          emergencyContacts: []
                         }
-                    },
-                    templateUrl: 'clientregister/clientregister.tpl.html',
-                    controller: 'ClientRegisterController',
-                    controllerAs: 'CR'
+                      };
+                    }
+                  },
+                  templateUrl: 'clientregister/clientregister.tpl.html',
+                  controller: 'ClientRegisterController',
+                  controllerAs: 'CR'
                 })
 
                 .state('editClient', {
-                    //  authenticate: true,
-                    url: '/editClient/:id',
-                    data: {pageTitle: 'Edit Client'},
-                    resolve: {
-                        resolvedClient: function (ClientService, $stateParams) {
-                            return ClientService.get({id: $stateParams.id}).$promise;
-                        }
-                    },
-                    templateUrl: 'clientregister/clientregister.tpl.html',
-                    controller: 'ClientRegisterController',
-                    controllerAs: 'CR'
+                  //  authenticate: true,
+                  url: '/editClient/:id',
+                  data: {pageTitle: 'Edit Client'},
+                  resolve: {
+                    resolvedClient: function (ClientService, $stateParams) {
+                      return ClientService.get({id: $stateParams.id}).$promise;
+                    }
+                  },
+                  templateUrl: 'clientregister/clientregister.tpl.html',
+                  controller: 'ClientRegisterController',
+                  controllerAs: 'CR'
                 });
         }]);
 })();
