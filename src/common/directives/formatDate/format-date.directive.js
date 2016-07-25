c5ba2d6e2f2cbd55e791156e4995dcd91c4e3248
Kevin Chan <kvii.chan@gmail.com>
2016-07-25 14:40:46 -0400

    initial commit for hotfixes to develop

--- src/common/directives/formatDate/format-date.directive.js
+++ src/common/directives/formatDate/format-date.directive.js
@@ -12,22 +12,42 @@
     .module('dados.common.directives.formatDate', [])
     .directive('formatDate', formatDate);
 
-  formatDate.$inject = [];
+  formatDate.$inject = ['$parse'];
 
-  function formatDate() {
-    return {
-      require: 'ngModel',
-      link: function(scope, elem, attr, modelCtrl) {
-        modelCtrl.$formatters.push(function(modelValue) {
-          if (modelValue) {
-            return new Date(modelValue);
-          }
-          else {
-            return null;
-          }
-        });
-      }
+  function formatDate($parse) {
+    var directive = {
+      restrict: 'A',
+      require: ['ngModel'],
+      link: link
     };
+
+    return directive;
+
+    ///////////////////////////////////////////////////////////////////////////
+
+    function link(scope, element, attr, ctrls) {
+      var ngModelController = ctrls[0];
+
+      // called with a JavaScript Date object when picked from the datepicker
+      ngModelController.$parsers.push(function (viewValue) {
+        // undo the timezone adjustment we did during the formatting
+        viewValue.setMinutes(viewValue.getMinutes() - viewValue.getTimezoneOffset());
+        // we just want a local date in ISO format
+        return viewValue.toISOString().substring(0, 10);
+      });
+
+      // called with a 'yyyy-mm-dd' string to format
+      ngModelController.$formatters.push(function (modelValue) {
+        if (!modelValue) {
+          return undefined;
+        }
+        // date constructor will apply timezone deviations from UTC (i.e. if locale is behind UTC 'dt' will be one day behind)
+        var datetime = new Date(modelValue);
+        // 'undo' the timezone offset again (so we end up on the original date again)
+        datetime.setMinutes(datetime.getMinutes() + datetime.getTimezoneOffset());
+        return datetime;
+      });
+    }
   }
 
 })();
